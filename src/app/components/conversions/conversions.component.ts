a1dddc2653fa0c6fb871d4096ebc1e8910c24833
Tim White <twhite@stewarteng.local>
2019-02-25 13:20:14 -0500

    added jquery, created table display of units with visibility toggle

--- src/app/components/conversions/conversions.component.ts
+++ src/app/components/conversions/conversions.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UNITS } from './units';
+import { Unit } from './unit';
+declare var $: any;
 
 @Component({
   selector: 'app-conversions',
@@ -9,10 +12,16 @@ export class ConversionsComponent implements OnInit {
 
 
   /* TODO: Add conversion function */
+  units = UNITS;
+
+  public hideUnitsTable() {
+    $('#unitsTable').toggle();
+  }
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
