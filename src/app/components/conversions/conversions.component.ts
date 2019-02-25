4366cec3051ae061224abf4b92d1c77609baeefe
Tim White <twhite@stewarteng.local>
2019-02-25 14:23:53 -0500

    WIP: input parser for conversion factors, factor lookup, etc

--- src/app/components/conversions/conversions.component.ts
+++ src/app/components/conversions/conversions.component.ts
@@ -14,6 +14,14 @@ export class ConversionsComponent implements OnInit {
   /* TODO: Add conversion function */
   units = UNITS;
 
+  /* Work-in-progress: Input parser for conversion, conversion lookup, rate finder */
+  public onConversionChange(input: string) {
+    var conversionUnit = input.split(" ").pop();
+    if(conversionUnit.length > 0){
+      var cFactor = this.units.find( x => x.abbr === conversionUnit).factor;
+    }
+    console.log(cFactor);
+  }
 
 
   constructor() { }
