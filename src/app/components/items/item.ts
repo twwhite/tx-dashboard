f025f2c5a69673600dc3687e2e7b9f592ab92cbf
Tim White <twhite@stewarteng.local>
2019-02-25 09:20:01 -0500

    define class Item in item.ts

---
+++ src/app/components/items/item.ts
@@ -0,0 +1,19 @@
+export class Item {
+
+    // An item is a concept of something that can be transacted. It is a placeholder that has no inherint value, only an expected value per quantity
+
+    // Mandatory inputs
+    type: number;       // 0 = buyable, 1 = sellable, 2 = buyable/sellable
+    id: number;         // Unique identifier to one item. Implement random generation?
+    name: string;       // Display name for item
+    category: string;   // Category of item, example "raw materials"
+    value: number;      // Number representation of value (Can be zero)
+    value_unit: string; // Unit representation of value (used with value input)
+
+    // Conditional inputs
+    margin?: number;    // Optional margin, enabled for sellable items (type 1 and 2) Fx = ABS(value)*(1+margin)
+    yield?: number;     // Can product be lost during creation? Yield = 0 .. 1
+
+
+  }
+  
