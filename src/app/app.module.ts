5f0fba415d98ff26e149311b7611e0bb5d44e38d
Tim White <twhite@stewarteng.local>
2019-02-25 09:01:34 -0500

    Add items, conversions, and transactios components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { ConversionsComponent } from './conversions/conversions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    TransactionsComponent,
+    ConversionsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/conversions/conversions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conversions',
+  templateUrl: './conversions.component.html',
+  styleUrls: ['./conversions.component.scss']
+})
+export class ConversionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
