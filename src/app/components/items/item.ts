b40f448272d41ef2dacd87e6ce82f13682fc1ac5
Tim White <twhite@stewarteng.local>
2019-02-25 16:34:48 -0500

    general notes and stuff

--- src/app/components/items/item.ts
+++ src/app/components/items/item.ts
@@ -16,7 +16,12 @@ export class Item {
     /* Conditional */ 
     margin?: number;    // Optional margin, enabled for sellable items (type 1 and 2) Fx = ABS(value)*(1+margin)
     yield?: number;     // Can product be lost during creation? Yield = 0 .. 1
-    dependencies?: [number, number, string][]; 
+    dependencies?: [number, number, string][]; // Items that must be costed to use this item.
+    // TODO: Request confirmation a dependency is inserted alongside another dependcy that already has that new dependency as a subdependency
+    /*
+        e.g. item: Finished Window
+                dependencies: [Wood, Glass, Sand (ALERT - Dependency: Glass already contains Dependency: Sand are you sure you wish to add?)]
+    */
 
 }
   
