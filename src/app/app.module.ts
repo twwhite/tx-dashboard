ec3c605fe2a257e9f7486d3c4158221507976c9b
Tim White <twhite@stewarteng.local>
2019-02-25 09:05:59 -0500

    mv components to /components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ItemsComponent } from './items/items.component';
-import { TransactionsComponent } from './transactions/transactions.component';
-import { ConversionsComponent } from './conversions/conversions.component';
+import { ItemsComponent } from './components/items/items.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+import { ConversionsComponent } from './components/conversions/conversions.component';
 
 @NgModule({
   declarations: [
